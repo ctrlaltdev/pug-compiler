eca72539f74f6028af59f6648ab9327274bd175f
Sascha <yorick@outlook.com>
2016-11-01 20:51:17 +0100

    it wooorks, it's aliiiiiive.

--- pug-compiler.js
+++ pug-compiler.js
@@ -1,2 +1,22 @@
 #!/usr/bin/env node
-var pug = require('pug');
+var pug = require('pug'),
+    fs = require('fs'),
+    path = './pug/';
+
+function compilePug(files){
+  files.map(function(file){
+    var fn = pug.compileFile(path+file, {pretty: '\t'});
+    var html = fn();
+    fs.writeFile(file.replace('.pug', '.html'), html, 'utf8', (err) => {
+      if (err) throw err;
+      console.log(file+' compiled.\n');
+    });
+  });
+}
+
+fs.readdir(path, 'utf8', function (err, files){
+  if (err) {
+    return console.log(err);
+  }
+  compilePug(files);
+});
